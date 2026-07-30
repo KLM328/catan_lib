7fa985b3760c1e56bfd8eed9cc84bddb3b8de0f2
quentin-cillierre <quentin.cillierre@imt-atlantique.net>
2026-07-30 09:15:32 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+mod resource;
+mod board;
+mod roll;
+mod player;
+mod game;
+
+pub use resource::{Resource, Cost, ResourceCounts, Hand};
+pub use board::tile::{NumberToken, Tile};
+pub use roll::Roll;
+pub use board::Board;
+pub use player::Player;
+pub use game::Game;
+
+
+
+
+
+
+
+
+
